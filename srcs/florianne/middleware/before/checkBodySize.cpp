1b77e321db25a866bf8425844efe63cbe7d46e08
fcoudert <fcoudert@student.42.fr>
2021-07-26 14:43:13 +0200

    max body size check  🐨

---
+++ srcs/florianne/middleware/before/checkBodySize.cpp
@@ -0,0 +1,28 @@
+#include "checkBodySize.hpp"
+// #include "G"
+// #include "File.hpp"
+
+CheckBodySize::~CheckBodySize() {
+}
+
+void CheckBodySize::handle(ClientSocket &client,Config &config, Request &request, Response &response, MiddlewareChain &next) {
+	
+	// File fileGet(request.getUrl());
+	(void)client;
+	(void)response;
+	(void)request;
+	(void)config;
+	
+	if(response.getStatus() == 200)
+	{
+		if(request.getBody().size() > config.getBodyMaxSize(client.getServerName(), request.getUrl()))
+			response.setStatus(413);
+
+
+
+		// std::cout << "\nSIZE BODY =" << request.getBody().size();
+		// std::cout << "\nMAX SIZE =" << config.getBodyMaxSize(client.getServerName(), request.getUrl());
+	}
+
+	next();
+}
--- srcs/florianne/middleware/before/checkRoot.cpp
+++ srcs/florianne/middleware/before/checkRoot.cpp
@@ -16,7 +16,7 @@ void CheckRoot::handle(ClientSocket &client, Config &config,Request &request, Re
 
 	if(response.getStatus() == 200)
 	{
-		std::cout << "CHECK ROOT" << std::endl;
+		// std::cout << "CHECK ROOT" << std::endl;
 		std::map <std::string, std::string> parsedUri = request.getParsedUri();
 		usable<std::pair<std::string, std::string> > root;
 		
@@ -29,10 +29,10 @@ void CheckRoot::handle(ClientSocket &client, Config &config,Request &request, Re
 			new_second.insert(new_second.size(), "/");
 			newe.replace(newe.find(root.value.first),root.value.first.length(),new_second);
 			request.setUrl(newe);
-  			std::cout << "\nURL =" << parsedUri.find("path")->second;
-			std::cout << "\nSTRING 1 =" << root.value.first;
-			std::cout << "\nSTRING 2 =" << root.value.second;
-			std::cout << "\nNEW = " << newe << '\n';
+  			// std::cout << "\nURL =" << parsedUri.find("path")->second;
+			// std::cout << "\nSTRING 1 =" << root.value.first;
+			// std::cout << "\nSTRING 2 =" << root.value.second;
+			// std::cout << "\nNEW = " << newe << '\n';
 		}
 	}
 	next();
